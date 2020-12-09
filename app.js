c1bb821c3e2d6eea40ca8fcced593206f02c89f8
Grace Kayode <44105901+12Grace12@users.noreply.github.com>
2020-12-08 23:50:01 -0500

    added files

---
+++ app.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://12grace12:mickey1219@cluster0.qw272.mongodb.net/?retryWrites=true&w=majority";
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("companies");
+  fs.createReadStream('companies-1.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      var myobj = row;
+      dbo.collection("companies").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("A company has been successfully inserted");
+        db.close();
+      });
+    })
+});
+
+
